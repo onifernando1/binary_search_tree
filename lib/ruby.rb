3d9c10f8346ab78faa2ce58645890d34a7b30502
onifernando1 <onifernando1@gmail.com>
2022-07-27 16:13:15 +0100

    Sort and remove duplicates function

--- lib/ruby.rb
+++ lib/ruby.rb
@@ -11,5 +11,36 @@ end
 
 
 class Tree
+    
+    def initialize
+
+    end 
+
+    def sort_and_remove_duplicates(array)
+        unsorted_array = array
+        new_array = []
+        unsorted_array.each do |item|
+            if new_array.include?(item) == false 
+                new_array << item
+            end 
+        end 
+        
+        new_array = new_array.sort()
+        
+    
+    end 
+
+    def build_tree(array)
+        new_array = sort_and_remove_duplicates(array)
+    end 
+
+
+    
+
+  
 
 end 
+
+tree = Tree.new()
+a = [8, 6, 1, 1, 2, 3, 4, 5, 6, 3]
+tree.build_tree(a)
