a14ea93181b99c0bd81be75366f1716287e7a97c
onifernando1 <onifernando1@gmail.com>
2022-07-28 00:18:10 +0100

    Get pretty print to workj

--- lib/ruby.rb
+++ lib/ruby.rb
@@ -1,12 +1,12 @@
 class Node
 
 
-    attr_accessor :left_children, :right_children, :data
+    attr_accessor :left, :right, :data
 
-    def initialize(data, left_children=nil, right_children=nil)
+    def initialize(data)
         @data = data
-        @left_children = left_children
-        @right_children = right_children
+        @left = nil
+        @right = nil
     end 
 
 
@@ -15,11 +15,11 @@ end
 
 class Tree
 
-    attr_accessor :sorted_array
+    attr_accessor :sorted_array, :root, :left_children, :right_children
     
     def initialize(array)
         @sorted_array = array.uniq.sort()
-        # @root = build_tree(@sorted_array)
+        @root = build_tree(@sorted_array)
 
     end 
 
@@ -27,36 +27,43 @@ class Tree
     def build_tree(array)
 
         #base case 
-        if array.empty?
-            return nil 
-        else
+        return nil if array.empty?
     
         #get middle of array and make it the root 
-        middle = (array.size - 1) / 2
-        @root_node = Node.new(array[middle])
+        middle = (array.length - 1) / 2
+        root_node = Node.new(array[middle])
 
         #construct left subtree recursively and make it the left child of original root 
-        @root_node.left_children = build_tree(array[0...middle])
-        @root_node.right_children = build_tree(array[(middle+1)..-1])
-        @root_node 
-        end 
+        root_node.left = build_tree(array[0...middle])
+        root_node.right = build_tree(array[(middle+1)..-1])
+        p root_node.data
+        p root_node
+        root_node 
+        #why does it not work with @ but it works without 
+        
 
     end 
 
-    def pretty_print(node = @root_node, prefix = '', is_left = true)
-        pretty_print(node.right_children, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_children
+    def pretty_print(node = @root, prefix = '', is_left = true)
+        pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
         puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-        pretty_print(node.left_children, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_children
+        pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
     end
     
+    def insert 
+
+    end 
+
+
+    def delete 
+
+    end 
 
   
 
 end 
 
-a = [8, 6, 1, 1, 2, 3, 4, 5, 6, 3]
-tree = Tree.new(a)
-sorted_array = tree.sorted_array
-tree.build_tree(sorted_array)
-tree.pretty_print
-
+array = [6,212,23,2,3,4,5,6,7,8,9,0,9,7,5,3123,12]
+tree = Tree.new(array)
+tree.build_tree(array)
+tree.pretty_print()
