c6b2249ed47a7ccefa215d10b5f8952825cfcd3c
onifernando1 <onifernando1@gmail.com>
2022-07-27 23:08:20 +0100

    Simplify sorrting and uniq

--- lib/ruby.rb
+++ lib/ruby.rb
@@ -1,7 +1,7 @@
 class Node
 
 
-    attr_accessor :left_children, :right_children
+    attr_accessor :left_children, :right_children, :data
 
     def initialize(data)
         @data = data
@@ -18,24 +18,11 @@ class Tree
     attr_accessor :sorted_array
     
     def initialize(array)
-        @root = nil #change
-        @sorted_array = sort_and_remove_duplicates(array)
+        @sorted_array = array.uniq.sort()
+        @root = build_tree(@sorted_array, 0, @sorted_array.length - 1)
 
     end 
 
-    def sort_and_remove_duplicates(array)
-        unsorted_array = array
-        new_array = []
-        unsorted_array.each do |item|
-            if new_array.include?(item) == false 
-                new_array << item
-            end 
-        end 
-        
-        @sorted_array = new_array.sort()
-        @sorted_array
-    
-    end 
 
     def build_tree(array, start, ending)
 
@@ -46,17 +33,21 @@ class Tree
     
         #get middle of array and make it the root 
         middle = (start + ending) / 2
-        root = Node.new(array[middle])
+        @root_node = Node.new(array[middle])
 
         #construct left subtree recursively and make it the left child of original root 
-        root.left_children = build_tree(array, start, middle - 1 )
-        root.right_children = build_tree(array, middle + 1, ending)
-        root 
+        @root_node.left_children = build_tree(array, start, middle - 1 )
+        @root_node.right_children = build_tree(array, middle + 1, ending)
+        @root_node 
         end 
 
     end 
 
-
+    def pretty_print(node = @root, prefix = '', is_left = true)
+        pretty_print(node.right_children, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_children
+        puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+        pretty_print(node.left_children, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_children
+    end
     
 
   
@@ -65,7 +56,9 @@ end
 
 a = [8, 6, 1, 1, 2, 3, 4, 5, 6, 3]
 tree = Tree.new(a)
-sorted_array = tree.sorted_array
-length = sorted_array.length
-tree.build_tree(sorted_array, 0, length - 1)
+p tree.sorted_array
+# sorted_array = tree.sorted_array
+# length = sorted_array.length
+# tree.build_tree(sorted_array, 0, length - 1)
+# tree.pretty_print
 
